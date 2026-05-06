868778c24321054fc44d7375237225dfe7607d75
JZTxBoboi <154489882+JZTxBoboi@users.noreply.github.com>
2026-05-06 23:48:33 +0800

    Delete rating directory

--- rating/src/data/technicians.js
+++
@@ -1,6 +0,0 @@
-
-export const technicians = [
-  { id: "T001", name: "Ahmad", img: "/technician1.jpg" },
-  { id: "T002", name: "Hafiz", img: "/technician2.jpg" },
-  { id: "T003", name: "Danial", img: "/technician3.jpg" }
-];
--- rating/src/utils/deviceHelpers.js
+++
@@ -1,7 +0,0 @@
-
-export const getDeviceType = () => {
-  const ua = navigator.userAgent;
-  if (/mobile/i.test(ua)) return "Mobile";
-  if (/tablet/i.test(ua)) return "Tablet";
-  return "Desktop";
-};
